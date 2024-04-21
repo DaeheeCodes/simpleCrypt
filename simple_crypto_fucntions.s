8dc75dd85a831e7c1dc37dd5f45f6a7096424280
Daehee Hwang <daeheecodes@gmail.com>
2024-04-21 06:00:19 +0000

    tested, complete for requirements

--- simple_crypto_fucntions.s
+++ simple_crypto_fucntions.s
@@ -13,43 +13,43 @@
 .align 4
 encrypt:
 // step #1_en - Save R4,R5,R6,R7, and LR
-PUSH {R4-R7, LR}
+push { r4, r5, r6, r7, r14 }
 // step #2_en - Set register R4 to 0
-MOV R4, #0
-// step #3_en - Set register R5 to 0
-MOV R5, #0
+mov r4, #0
+// step #3+_en - Set register R5 to 0
+mov r5, #0
 EN_WHL_R5_LT_R1:
 // step #4_en - compare R5 and R1 in this order of the registers
-CMP R5, R1
+cmp r5, r1
 // step #5_en - if R5 greater than or equal to R1, branch to EN_END_WHL_R5_LT_R1
-BGE EN_END_WHL_R5_LT_R1
+// bge = greater, equal conditioanl
+bge EN_END_WHL_R5_LT_R1
 // step #6_en - load byte into register R6, the contents of R0[R5]
-LDRB R6, [R0, R5]
+ldrb r6, [r0,r5]
 // step #7_en - load byte into register R7, the contents of R2[R4]
-LDRB R7, [R2, R4]
+ldrb r7, [r2,r4]
 // step #8_en - exclusive or R6 and R7, storing result in R6
-EOR R6, R7
+eor r6,r7
 // step #9_en - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store in R7
-AND R7, R5, #255
+and r7,r5, #255
 // step #10_en - add R6 and R7, storing result in R6
-ADD R6, R7
+add r6,r7
 // step #11_en - store byte in R6 to R0[R5]
-STRB R6, [R0, R5]
+strb r6, [r0,r5]
 // step #12_en - increment R4 by one
-ADD R4, #1
+add r4, #1
 EN_IF_R4_GE_R3:
 // step #13_en - compare R4 and R3 in this order of the registers
-CMP R4, R3
+cmp r4, r3
 // step #14_en - do conditional execution, move if greater than or equal, into R4 the value #0
-MOVGE R4, #0
+movge r4, #0
 // step #15_en - increment R5 by one
-ADD R5, #1
+add r5, #1
 // step #16_en - branch (or branch ALways) back to EN_WHL_R5_LT_R1 label
-B EN_WHL_R5_LT_R1
+b EN_WHL_R5_LT_R1
 EN_END_WHL_R5_LT_R1:
 // step #17_en - restore the registers R4,R5,R6,R7, and PC; in the order of registers given
-POP {R4-R7, PC}
-
+pop {r4,r5,r6,r7, r14}
 //////////////////////////////////////////////////////////////////////////////////////////////
 /// end of encrypt function //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,43 +64,43 @@ POP {R4-R7, PC}
 .align 4
 decrypt:
 // step #1_de - Save R4,R5,R6,R7, and LR
-PUSH {R4-R7, LR}
+push {r4,r5,r6,r7, r14}
 // step #2_de - Set register R4 to 0
-MOV R4, #0
-// step #3_de - Set register R5 to 0
-MOV R5, #0
+mov r4, #0
+// step #3+_de - Set register R5 to 0
+mov r5, #0
 DE_WHL_R5_LT_R1:
 // step #4_de - compare R5 and R1 in this order of the registers
-CMP R5, R1
-// step #5_de - if R5 greater than or equal to R1, branch to DE_END_WHL_R5_LT_R1
-BGE DE_END_WHL_R5_LT_R1
+cmp r5, r1
+// step #5_de - if R5 greater than or equal to R1, branch to EN_END_WHL_R5_LT_R1
+// bge = greater, equal conditioanl
+bge EN_END_WHL_R5_LT_R1
 // step #6_de - load byte into register R6, the contents of R0[R5]
-LDRB R6, [R0, R5]
+ldrb r7, [r0, r5]
 // step #7_de - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store in R7
-AND R7, R5, #255
+and r7, r5, #255
 // step #8_de - subtract R6 and R7, storing result in R6
-SUB R6, R7
+sub r6, r7
 // step #9_de - load byte into register R7, the contents of R2[R4]
-LDRB R7, [R2, R4]
+ldrb r7, [r2, r4]
 // step #10_de - exclusive or R6 and R7, storing result in R6
-EOR R6, R7
+eor r6, r7
 // step #11_de - store byte in R6 to R0[R5]
-STRB R6, [R0, R5]
+strb r6, [r0,r5]
 // step #12_de - increment R4 by one
-ADD R4, #1
+add r4, #0
 DE_IF_R4_GE_R3:
 // step #13_de - compare R4 and R3 in this order of the registers
-CMP R4, R3
+cmp r4, r3
 // step #14_de - do conditional execution, move if greater than or equal, into R4 the value #0
-MOVGE R4, #0
+movge r4, #0
 // step #15_de - increment R5 by one
-ADD R5, #1
+add r5, #1
 // step #16_de - branch (or branch ALways) back to DE_WHL_R5_LT_R1 label
-B DE_WHL_R5_LT_R1
+b DE_WHL_R5_LT_R1
 DE_END_WHL_R5_LT_R1:
 // step #17_en - restore the registers R4,R5,R6,R7, and PC; in the order of registers given
-POP {R4-R7, PC}
-
+pop {r4,r5,r6,r7, r14}
 //////////////////////////////////////////////////////////////////////////////////////////////
 /// end of decrypt function //////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////]
